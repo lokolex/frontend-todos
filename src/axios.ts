7e288065701c1bc2015c4e8fbdd0f5706c4d198a
Aleksey Strizhenko <lokolex310188@gmail.com>
2023-09-21 12:31:50 +0400

    Edited axios config

--- src/axios.ts
+++ src/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://alexstr-todos.onrender.com',
+  baseURL: 'https://p5555-z0808c543-z7c7a7d5a-gtw.zb20e5e48.qovery.fr',
 });
 
 instance.interceptors.request.use((config) => {
